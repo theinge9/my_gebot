4bc08a858c2ba0ae10ee82e3f0fa838db631dab5
Theinge9 <gold.cold17@gmail.com>
2024-04-22 13:06:00 +0900

    Updated on 0422

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -9,22 +9,21 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    package_name='my_gebot'
-    rviz_config_path = os.path.join(get_package_share_directory(package_name), 'rviz/urdf.rviz')
-    #world_path = os.path.join(get_package_share_directory(package_name), 'worlds/test1.world')
+    pkg_name = os.path.join(get_package_share_directory('my_gebot'))
+    rviz_config_path = os.path.join(pkg_name, 'rviz/urdf.rviz')
+    world_path = os.path.join(pkg_name, 'worlds', 'test1.world')
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+                PythonLaunchDescriptionSource([os.path.join(pkg_name, 'launch', 'rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
-                )]), #launch_arguments={'world': 'world_path'}.items()
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py'
+                ]), launch_arguments={'world': world_path}.items()
     )
-
+    
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'gebot'],
@@ -49,10 +48,10 @@ def generate_launch_description():
         DeclareLaunchArgument(
             name='rvizconfig', default_value=rviz_config_path, 
             description='Absolute path to rviz config file'),
-
         rsp,
         gazebo,
         spawn_entity,
         rviz_node,
-        #control_drive_spawner
+        #control_drive_spawner,
+        #gzserver, gzclient
     ])
